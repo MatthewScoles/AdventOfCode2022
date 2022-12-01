b6bcf3771588540eb9ac49eccc271282fc535322
MatthewScoles <msscoles@stu.naperville203.org>
2022-12-01 14:19:29 -0600

    Account and Banking

---
+++ AdventOfCode2022/Day_1.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Day_1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Day_1
+{
+    public static void main(String[]args)
+    {
+        
+    }
+}
